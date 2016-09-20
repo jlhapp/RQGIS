335c14f3d00a4199cdae93e5f494fd0cd885f1f2
jlhapp <jena.happ@duke.edu>
2016-09-20 10:11:31 -0400

    Example of tools from QGIS

--- Setting_Up.R
+++ Setting_Up.R
@@ -1,4 +1,5 @@
-#To use RQGIS package, you need to know the parameters
+#RQGIS package allows users to use tools from QGIS.
+
 #Find available tools here:
 #C:\Program Files\QGIS 2.14\apps\qgis\python\plugins\processing\algs\qgis
 #C:\Program Files\QGIS 2.14\apps\qgis\python\plugins\GdalTools\tools
@@ -24,3 +25,7 @@ my_env$qt4 <- "C:\\Program Files\\QGIS 2.14\\apps\\Qt4"
 my_env$python27 <- "C:\\Program Files\\QGIS 2.14\\apps\\Python27"
 my_env$grass <- "C:\\Program Files\\QGIS 2.14\\apps\\grass"
 my_env$msys <- "C:\\Program Files\\QGIS 2.14\\apps\\msys"
+
+#Search for function names.  Change the search term as needed.  It will 
+#provide the folder name and name of the tool that you will use below.
+find_algorithms(search_term = "add", qgis_env = my_env)
